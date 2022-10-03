ec84d87d2f6c62169d2b0474ea1c1dfdb6eeb392
Dafarlog <Dafarlog@gmail.com>
2022-10-03 15:32:36 +0300

    add section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
 import { User } from "./User/User";
 import { UsersList } from "./UsersList/UsersList";
+import { Section } from "./Section/Section";
 import {users} from "data/users"
 
 export const App = () => {
   return (
   <>
-    <User user={users[0]}/>
-    <UsersList users={users}/>
+    <Section>
+      <User user={users[0]}/>
+    </Section>
+    <Section title ="userslist">
+      <UsersList users={users}/>
+    </Section>   
   </>
   );
 };
