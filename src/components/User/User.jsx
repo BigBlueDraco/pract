b62df7cad1eae79ea8f59b694188888abd053448
Dafarlog <Dafarlog@gmail.com>
2022-10-03 14:38:37 +0300

    add propTypes to Users

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,3 +1,17 @@
-export default function User(){
+import PropTypes from 'prop-types'; 
+export const User = ({user: {name, email}}) =>{
     
+    return (
+        <>
+            <p>Name: {name}</p>
+            <p>Email: {email}</p>
+        </>
+    );
+}
+
+User.propTypes = {
+    user: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired,
+    }).isRequired
 }
